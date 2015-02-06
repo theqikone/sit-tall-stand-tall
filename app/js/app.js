9bca6a83a58e78fb6074ee59af979096889f88c6
Sione <sinoke@izeni.com>
2015-02-06 12:51:26 -0700

    Found and applied the background image. Played around with using a background video, but didn't like the way it turned out.

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,8 @@
+$(document).ready(function() {
+  $('.background-image').on('webkitAnimationEnd', function(e) {
+    $(this).addClass('visible');
+  });
+});
+
+
 var app = angular.module('myApp', ['ui.router', 'mgcrea.ngStrap']);
